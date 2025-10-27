c9920111f043530e29e508bdd19ef132de15bc0e
jwo0o0 <jwo0o0358@gmail.com>
2025-10-27 11:49:06 +0900

    chore: prettier 적용

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
+import GlobalStyle from '@styles/GlobalStyle';
+import '@styles/font.css';
+import theme from '@styles/theme';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
-import '@styles/font.css';
 import { ThemeProvider } from 'styled-components';
-import theme from '@styles/theme';
-import GlobalStyle from '@styles/GlobalStyle';
+
+import App from './App.jsx';
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
