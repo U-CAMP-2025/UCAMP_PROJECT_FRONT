c0e6d7e1058d94c23bc1cb21b25a29a15f4f795d
jwo0o0 <jwo0o0358@gmail.com>
2025-10-29 16:57:02 +0900

    feat: 관리자 페이지 컴포넌트, 라우터 추가

---
+++ src/pages/Admin/Transcription/index.jsx
@@ -0,0 +1,9 @@
+import { PageContainer } from '@components/layout/PageContainer';
+
+export default function AdminTranscriptionPage() {
+  return (
+    <PageContainer header footer>
+      <div>관리자용 전사 페이지</div>
+    </PageContainer>
+  );
+}
