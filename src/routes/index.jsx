402a9b8cd22274985948930bf9d9f74332248268
HongSaek-Ko <bh1152@naver.com>
2025-10-29 09:43:45 +0900

    Feat/#6 (#8)
    * 폼 생성
    
    * react-hook-form 추가
    Page 조정
    
    * 폼 수정

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,7 @@
 import LandingPage from '@pages/Landing';
 import NotFoundPage from '@pages/NotFound';
 import TestPage from '@pages/Test';
+import RegistPage from '@pages/Users/RegistPage';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
 const AppRoutes = () => {
@@ -9,6 +10,7 @@ const AppRoutes = () => {
       <Routes>
         <Route path='/' element={<LandingPage />} />
         <Route path='/test' element={<TestPage />} />
+        <Route path='/admin' element={<RegistPage />} />
         <Route path='*' element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
