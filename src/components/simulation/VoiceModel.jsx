df106ba75e8ee2fa5796f9d63e3c16c4dae5023c
Ahn-JuHwan <ajuh1103@naver.com>
2025-10-30 13:29:13 +0900

    Voice Model 분리

---
+++ src/components/simulation/VoiceModel.jsx
@@ -0,0 +1,106 @@
+const VoiceModel = [
+  // "홀수" : 남자 WzMnDIgiICcj1oXbUBO0
+  // "짝수" : 여자 ksaI0TCD9BstzEzlxj4q
+  {
+    // 01
+    voiceId: 'WzMnDIgiICcj1oXbUBO0',
+    voice_settings: {
+      stability: 0.5,
+      similarity_boost: 0.8,
+      style: 0.3,
+      use_speaker_boost: true,
+    },
+  },
+  {
+    // 02
+    voiceId: 'ksaI0TCD9BstzEzlxj4q',
+    voice_settings: {
+      stability: 0.5,
+      similarity_boost: 0.8,
+      style: 0.3,
+      use_speaker_boost: true,
+    },
+  },
+  {
+    // 03
+    voiceId: 'WzMnDIgiICcj1oXbUBO0',
+    voice_settings: {
+      stability: 0.5,
+      similarity_boost: 0.8,
+      style: 0.3,
+      use_speaker_boost: true,
+    },
+  },
+  {
+    // 04
+    voiceId: 'ksaI0TCD9BstzEzlxj4q',
+    voice_settings: {
+      stability: 0.5,
+      similarity_boost: 0.8,
+      style: 0.3,
+      use_speaker_boost: true,
+    },
+  },
+  {
+    // 05
+    voiceId: 'WzMnDIgiICcj1oXbUBO0',
+    voice_settings: {
+      stability: 0.5,
+      similarity_boost: 0.8,
+      style: 0.3,
+      use_speaker_boost: true,
+    },
+  },
+  {
+    // 06
+    voiceId: 'ksaI0TCD9BstzEzlxj4q',
+    voice_settings: {
+      stability: 0.5,
+      similarity_boost: 0.8,
+      style: 0.3,
+      use_speaker_boost: true,
+    },
+  },
+  {
+    // 07
+    voiceId: 'WzMnDIgiICcj1oXbUBO0',
+    voice_settings: {
+      stability: 0.5,
+      similarity_boost: 0.8,
+      style: 0.3,
+      use_speaker_boost: true,
+    },
+  },
+  {
+    // 08
+    voiceId: 'ksaI0TCD9BstzEzlxj4q',
+    voice_settings: {
+      stability: 0.5,
+      similarity_boost: 0.8,
+      style: 0.3,
+      use_speaker_boost: true,
+    },
+  },
+  {
+    // 09
+    voiceId: 'WzMnDIgiICcj1oXbUBO0',
+    voice_settings: {
+      stability: 0.5,
+      similarity_boost: 0.8,
+      style: 0.3,
+      use_speaker_boost: true,
+    },
+  },
+  {
+    // 10
+    voiceId: 'ksaI0TCD9BstzEzlxj4q',
+    voice_settings: {
+      stability: 0.5,
+      similarity_boost: 0.8,
+      style: 0.3,
+      use_speaker_boost: true,
+    },
+  },
+];
+
+export default VoiceModel;
