ef7f4fa0787f99139e1f7de957e534fa6e4f1c1a
jh0008 <juhyeon3089@gmail.com>
2025-11-02 17:02:51 +0900

    feat: 시뮬레이션 결과(변환 후) 페이지 UI 구현

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -12,6 +12,7 @@ import SimulationPage from '@pages/Simulation';
 import SimulationEndPage from '@pages/Simulation/SimulationEnd';
 import SimulationGO from '@pages/Simulation/SimulationGo';
 import SimulationRecordPage from '@pages/Simulation/SimulationRecord';
+import SimulationResultPage from '@pages/Simulation/SimulationResult';
 import TestPage from '@pages/Test';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
@@ -32,6 +33,7 @@ const AppRoutes = () => {
         <Route path='/simulation/:simulationId/start' element={<SimulationGO />} />
         <Route path='/simulation/:simulationId/end' element={<SimulationEndPage />} />
         <Route path='/simulation/record' element={<SimulationRecordPage />} />
+        <Route path='/simulation/:simulationId/result' element={<SimulationResultPage />} />
         <Route path='/admin/user' element={<AdminPage />} />
         <Route path='/admin/transcription' element={<AdminPage />} />
         <Route path='/login/bridge' element={<LoginBridge />} />
