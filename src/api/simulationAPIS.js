d05ef82de5ab5fdc79475ff6b27c18cad760d413
김정우 <jwo0o0358@gmail.com>
2025-11-06 13:16:55 +0900

    fix: 면접 연습 결과 선택해서 수정

---
+++ src/api/simulationAPIS.js
@@ -0,0 +1,16 @@
+import { axiosInstance } from './axios';
+
+export const fetchSimulationRecords = async () => {
+  const { data } = await axiosInstance.get('/simulation/records');
+  return data;
+};
+
+export const fetchSimulationResult = async (simulationId) => {
+  const { data } = await axiosInstance.get(`/simulation/${simulationId}/result`);
+  return data;
+};
+
+export const putSimulationFinalize = async (simulationId, body) => {
+  await axiosInstance.put(`/simulation/${simulationId}/finalize`, body);
+  return;
+};
