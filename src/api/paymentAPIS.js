6ac8e79102726e903f937baba44c303447131fe7
jh0008 <juhyeon3089@gmail.com>
2025-11-09 17:27:51 +0900

    feat: 시뮬레이션 3회 제한 추가

--- src/api/paymentAPIS.js
+++ src/api/paymentAPIS.js
@@ -4,6 +4,6 @@ import { axiosInstance } from './axios';
  * GET: 유저의 최근 결제 내역 조회
  */
 export const fetchUserPayment = async () => {
-  const { data } = axiosInstance.get('/payment');
+  const { data } = await axiosInstance.get('/payment');
   return data;
 };
