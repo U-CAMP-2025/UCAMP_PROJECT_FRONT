19282762abb16d178df16802db7a6938b9186469
김정우 <jwo0o0358@gmail.com>
2025-11-09 17:28:11 +0900

    feat: 결제 API 연동 및 UI 구현

--- src/api/paymentAPIS.js
+++ src/api/paymentAPIS.js
@@ -4,7 +4,14 @@ import { axiosInstance } from './axios';
  * GET: 유저의 최근 결제 내역 조회
  */
 export const fetchUserPayment = async () => {
-  const { data } = axiosInstance.get('/payment');
+  const { data } = await axiosInstance.get('/payment/history/me');
+  return data;
+};
+/**
+ * GET: 특정 유저의 전체 결제 내역
+ */
+export const fetchAllUserPayment = async () => {
+  const { data } = await axiosInstance.get('/payment/history');
   return data;
 };
 
@@ -12,6 +19,6 @@ export const fetchUserPayment = async () => {
  * POST: 우리 서버에 결제 요청
  */
 export const postPaymentConfirm = async (payment) => {
-  const { data } = axiosInstance.post('/payment/confirm', payment);
+  const { data } = await axiosInstance.post('/payment/confirm', payment);
   return data;
 };
