72084b01c8394141914c36664ac9d0ece01015a8
김정우 <jwo0o0358@gmail.com>
2025-11-11 13:43:00 +0900

    fix: vite plugin console 설정 변경

--- vite.config.js
+++ vite.config.js
@@ -4,21 +4,12 @@ import removeConsole from 'vite-plugin-remove-console';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => {
-  return {
-    plugins: [
-      react(),
-      tsconfigPaths(),
-      command === 'build' &&
-        removeConsole({
-          includes: ['log', 'warn', 'error'],
-        }),
-    ],
-    server: {
-      port: 3000,
-    },
-    resolve: {
-      alias: [{ find: '@', replacement: '/src' }],
-    },
-  };
+export default defineConfig({
+  plugins: [react(), tsconfigPaths(), removeConsole()],
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    alias: [{ find: '@', replacement: '/src' }],
+  },
 });
